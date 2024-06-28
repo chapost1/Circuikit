759193a650704fdecd0e1cdbad9c43b4d6e52d66
Shahar Tal <stalmail10@gmail.com>
2024-06-28 10:41:36 +0300

    added template for smc instructions

--- app/__init__.py
+++ app/__init__.py
@@ -16,9 +16,15 @@ class AppInterface:
     def __init__(self, write_message_fn: Callable[[str], None]):
         self.write_message_fn = write_message_fn
 
+        smc = SerialMonitorCallback(write_message_fn=write_message_fn)
+        # If you want any other app task to send message to the serial monitor
+        # Then, just add it's constructor some send fn param
+        # and then do like: service(some_send_fn=smc.send_message)
+        # and add logic in the service....
+
         self.subscribers: list[Task] = [
+            smc,
             ThingsBoardGateway(token=THINGSBOARD_TOKEN),
-            SerialMonitorCallback(write_message_fn=write_message_fn),
             GUI(),
         ]
 
