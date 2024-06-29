6a69cea5e7242b27cbbe4f8d478e196862d4244f
Shahar Tal <stalmail10@gmail.com>
2024-06-30 00:14:49 +0300

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="circuikit",
-    version="0.1.0",
+    version="0.2.0",
     description="A versatile tool for Arduino serial monitoring and interaction",
     author="Shachar Tal",
     author_email="stalmail10@gmail.com",
